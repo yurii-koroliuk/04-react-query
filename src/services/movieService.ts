c135e93f9270dbe544651c5ada968caac08c492d
Yurii <ikorolyuk78@gmail.com>
2025-10-15 15:11:50 +0300

    fix

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,28 +2,18 @@ import axios from 'axios';
 import type { MovieResponse } from '../types/movie';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
 export const fetchMovies = async (
   query: string,
-  page: number = 1,
-  useToken: boolean = false
+  page: number = 1
 ): Promise<MovieResponse> => {
-  const config = useToken
-    ? {
-        params: { query, page },
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-        },
-      }
-    : {
-        params: {
-          query,
-          page,
-          api_key: API_KEY,
-        },
-      };
-  const response = await axios.get(BASE_URL, config);
+  const response = await axios.get(BASE_URL, {
+    params: {
+      api_key: API_KEY,
+      query,
+      page,
+    },
+  });
   return response.data;
 };
